a6a2eac4d533d1eef55f845b6a20d47f94f3d5fe
aymane_B <aymanefast@gmail.com>
2024-07-06 22:42:23 +0100

    updating routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-if (auth()->check()) {
-    Route::get('/dashboard', function () {
-        // Only authenticated users can access this route
-        dump(auth()->user());
-        return view('dashboard');
-    });
-}
+
 Route::get('/', function () {
     return view('welcome');
 });
